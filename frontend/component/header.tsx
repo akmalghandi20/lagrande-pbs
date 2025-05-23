f0da76ba9c94978d70a7f851f1ccf4cf15bb2ccc
DayanuArrofi <dayanuarrofi@gmail.com>
2025-05-23 18:15:11 +0700

    dayan: menambahkan logo ke dalam header

--- frontend/component/header.tsx
+++ frontend/component/header.tsx
@@ -1,12 +1,13 @@
+import Image from "next/image";
+
+
 export default function Header() {
   return (
     <header style={{backgroundColor: '#f3f3f3', 
     padding: '1rem',
     display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
     }}>
-      <h1>WARMASKIN</h1>
+      <Image src="/warmaskin.png" alt="WARMASKIN Logo" width={50} height={50} />
     </header>
   );
 }
