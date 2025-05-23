05eb131465526c24ff41cae290b335a759f189ab
DayanuArrofi <dayanuarrofi@gmail.com>
2025-05-23 17:12:30 +0700

    dayan: memengedit tampilan header

--- frontend/component/header.tsx
+++ frontend/component/header.tsx
@@ -1,6 +1,11 @@
 export default function Header() {
   return (
-    <header>
+    <header style={{backgroundColor: '#f3f3f3', 
+    padding: '1rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    }}>
       <h1>WARMASKIN</h1>
     </header>
   );
