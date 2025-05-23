070de246bd625a1447b1f8303e451d978d972a63
DayanuArrofi <dayanuarrofi@gmail.com>
2025-05-23 17:46:46 +0700

    dayan: mengedit tammpilan navbar

--- frontend/component/navbar.tsx
+++ frontend/component/navbar.tsx
@@ -1,12 +1,18 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-  return (
-   <nav>
-    <Link href="/">Home</Link>
-    <Link href="/menu">Menu</Link>
-    <Link href="/pemesanan">Pemesanan</Link>
+  const pathname = usePathname();
 
-   </nav>
+  const linkclass = (path: string) =>
+    pathname === path ? "text-blue-500 font-bold" : "text-black";
+  return (
+    <nav style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
+      <Link href="/" className={linkclass('/')}>Home</Link>
+      <Link href="/menu" className={('/menu')}>Menu</Link>
+      <Link href="/pemesanan" className={('/pemesanan')}>Pemesanan</Link>
+    </nav>
   );
 }
