6666aea1b5271450498c0b6d8a1c8f3bb7e815cc
DayanuArrofi <dayanuarrofi@gmail.com>
2025-05-25 21:49:15 +0700

    dayan: membuat hover dan transform pada navbar

--- frontend/component/navbar.tsx
+++ frontend/component/navbar.tsx
@@ -1,18 +1,43 @@
-'use client'
-
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+
   const pathname = usePathname();
 
-  const linkclass = (path: string) =>
-    pathname === path ? "text-blue-500 font-bold" : "text-black";
-  return (
+  const linkClass = (path: string) => {
+    const isActive = pathname === path;
+    return `
+      relative 
+      pb-1 
+      text-gray-700 
+      hover:text-blue-600 
+      transition-colors 
+      duration-200
+      ${isActive ? "text-blue-600 after:w-full" : "after:w-0"}
+      after:absolute 
+      after:left-0 
+      after:-bottom-0.5 
+      after:h-0.5 
+      after:bg-blue-600 
+      after:transition-all 
+      after:duration-300 
+      hover:after:w-full
+    `;
+  };
+
+  return ( 
     <nav style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
-      <Link href="/" className={linkclass('/')}>Home</Link>
-      <Link href="/menu" className={('/menu')}>Menu</Link>
-      <Link href="/pemesanan" className={('/pemesanan')}>Pemesanan</Link>
+      <Link href="/"  className={linkClass("/")}>
+        Home
+      </Link>
+      <Link href="/menu">
+        Menu
+      </Link>
+      <Link href="/pemesanan">
+        Pemesanan
+      </Link>
     </nav>
   );
 }
