68b31d787dfe470214cbb187c82666fff05b02ac
DayanuArrofi <dayanuarrofi@gmail.com>
2025-05-25 20:21:05 +0700

    dayan: merubah layout navbar

--- frontend/component/header.tsx
+++ frontend/component/header.tsx
@@ -1,16 +1,33 @@
 import Image from "next/image";
 import Navbar from "./navbar";
 
-
 export default function Header() {
   return (
-    <header style={{backgroundColor: '#f3f3f3', 
-    padding: '1rem',
-    display: 'flex',
-    }}>
-      <Image src="/warmaskin.png" alt="WARMASKIN Logo" width={50} height={50} />
-      <div>
-                <Navbar />
+    <header
+      style={{ backgroundColor: "#f3f3f3", padding: "1rem" }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src="/warmaskin.png"
+          alt="WARMASKIN Logo"
+          width={50}
+          height={50}
+        />
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "2rem",
+          }}
+        >
+          <Navbar />
+        </div>
       </div>
     </header>
   );
