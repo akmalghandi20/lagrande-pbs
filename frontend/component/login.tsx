3f8e1e76238450b86e1ad8b0fb3209d3533d6da9
DayanuArrofi <dayanuarrofi@gmail.com>
2025-06-14 13:58:27 +0700

    dayan: membuat file login.tsx dan kerangka nya

---
+++ frontend/component/login.tsx
@@ -0,0 +1,15 @@
+export default function LoginForm() {
+  return (
+    <div>
+      <form>
+        <input type="text" 
+        />
+        <input type="text" 
+        />
+        <button>
+            login
+        </button>
+      </form>
+    </div>
+  );
+}
