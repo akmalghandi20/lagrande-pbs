9891c65314e8218c79816c4bbf0a0950b1db5b29
DayanuArrofi <dayanuarrofi@gmail.com>
2025-06-14 20:07:36 +0700

    dayan: menghapus datatest (untuk percobaan fungsi rekomendasi) di frontend karena akan digunakan pada cms

--- frontend/app/pemesanan/page.tsx
+++ frontend/app/pemesanan/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { menuList } from "@/datatest/menu"; // Pastikan menuList formatnya: { nama: string, harga: number }
 
 interface MenuItem {
   namaMenu: string;
