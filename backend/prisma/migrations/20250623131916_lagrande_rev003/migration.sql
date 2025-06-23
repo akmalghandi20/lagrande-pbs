c317b1691dbae437286bf5422ea8d44e61e0c6b0
akmalghandi20 <gandolkikok@gmail.com>
2025-06-23 20:20:31 +0700

    akmal : update database, migrate, dan memulai pengerjaan untuk fungsi tb_pesanan

---
+++ backend/prisma/migrations/20250623131916_lagrande_rev003/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `total_harga` on the `tb_pesanan` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `tb_pesanan` DROP COLUMN `total_harga`;
