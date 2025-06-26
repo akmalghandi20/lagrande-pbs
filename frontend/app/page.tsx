0283f073c55a9022b270559e2179eca704ad1295
DayanuArrofi <dayanuarrofi@gmail.com>
2025-06-26 22:38:18 +0700

    dayan: menambahkan css pada gambar dan deskripsi

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -15,20 +15,20 @@ export default function HomePage() {
           className="rounded-2xl shadow-lg object-cover w-full h-auto"
         />
       </div>
-      <div>
-        <h1>
+      <div className="w-full md:w-1/2 space-y-4">
+        <h1 className="text-4xl font-bold text-brown-800">
           Warmaskin Coffe
         </h1>
-        <p>
+        <p className="text-gray-700 text-lg">
           Nikmati secangkir kopi terbaik dari biji pilihan, diseduh dengan cinta di Warmaskin Coffee.
             Tempat nongkrong yang nyaman, cocok untuk kerja, santai, maupun kumpul bareng teman
         </p>
-        <div>
+        <div className="mt-4">
           <div>
-            <h2>
+            <h2 className="text-xl font-semibold text-brown-700">
               Jam Operasional
             </h2>
-            <ul>
+            <ul className="text-gray-600 text-base mt-1">
               <li>Senin - Sabtu : 08.00 - 10.00</li>
               <li>Minggu : 10.00 - 23.00</li>
             </ul>
