d5ea0672480fc476dd4d129cf017a89293541f7c
DayanuArrofi <dayanuarrofi@gmail.com>
2025-06-28 21:59:31 +0700

    dayan: membuat kerangka wadah menu, isian data berasal dari tambah menu yang dibuat cms

--- frontend/app/menu/page.tsx
+++ frontend/app/menu/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
 export default function MenuPage() {
   return (
     <div>
-      <h1>Menu</h1>
+      <h1>Daftar Menu</h1>
+      <div>
+        <div>
+          <div>
+            <span>
+              [Gambar Menu]
+            </span>
+          </div>
+          <div>
+            <h2>[Nama Menu]</h2>
+            <p>Rp[Harga Menu]</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
